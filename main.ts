75fed1a1d377b390054a9de706251bb25300dde7
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-15 19:59:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,14 @@
-pins.digitalWritePin(DigitalPin.P2, 1)
-pins.digitalWritePin(DigitalPin.P4, 1)
+function Zelena () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
+function Cervena () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+basic.forever(function () {
+    Cervena()
+    basic.pause(1000)
+    Zelena()
+    basic.pause(1000)
+})
