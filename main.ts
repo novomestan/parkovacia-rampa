58f915bdb1c758e4b43e3fa8e1ecf0d5ce35392d
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-15 20:01:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    Cervena()
+})
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
+input.onButtonPressed(Button.B, function () {
+    Zelena()
+})
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
 }
 basic.forever(function () {
-    Cervena()
-    basic.pause(1000)
-    Zelena()
-    basic.pause(1000)
+	
 })
