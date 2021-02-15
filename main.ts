2c2663e86f276f062b2c5b405940c91f8df21259
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-15 20:26:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    Cervena()
+    Zelena()
     wuKong.setServoAngel(wuKong.ServoList.S0, 45 / 1.5)
 })
 function Zelena () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 }
 input.onButtonPressed(Button.B, function () {
-    Zelena()
+    Cervena()
     wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 })
 function Cervena () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
 }
 wuKong.setServoAngel(wuKong.ServoList.S0, 0)
