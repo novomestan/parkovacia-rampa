e49ee1539cea7c500bbd856aeecf790fe8a39d26
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-15 20:07:22 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    Cervena()
+    Zelena()
+    wuKong.setServoAngel(wuKong.ServoList.S0, 0)
 })
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
 input.onButtonPressed(Button.B, function () {
-    Zelena()
+    Cervena()
+    wuKong.setServoAngel(wuKong.ServoList.S0, 180)
 })
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
 }
+wuKong.setServoAngel(wuKong.ServoList.S0, 0)
