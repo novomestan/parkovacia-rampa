56479d63248185e141d823b6c084e7cb47464e57
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-16 20:02:53 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 input.onButtonPressed(Button.A, function () {
     Cervena()
-    wuKong.setServoAngel(wuKong.ServoList.S0, 45 / 1.5)
 })
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 }
 input.onButtonPressed(Button.B, function () {
     Zelena()
-    wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 })
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    wuKong.setServoAngel(wuKong.ServoList.S0, 45 / 1.5)
 }
+let Vzdialenost = 0
 wuKong.setServoAngel(wuKong.ServoList.S0, 0)
+basic.forever(function () {
+    Vzdialenost = sonar.ping(
+    DigitalPin.P12,
+    DigitalPin.P13,
+    PingUnit.Centimeters
+    )
+    basic.pause(200)
+})
