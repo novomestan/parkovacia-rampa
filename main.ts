8772547f93773c3f85d241828708c9666027d854
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-16 20:17:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(100)
     wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 }
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(100)
     wuKong.setServoAngel(wuKong.ServoList.S0, 45 / 1.5)
 }
 let Vzdialenost = 0
@@ -16,6 +18,7 @@ basic.forever(function () {
     DigitalPin.P13,
     PingUnit.Centimeters
     )
+    basic.pause(100)
     if (Vzdialenost <= 20) {
         Zelena()
     } else {
