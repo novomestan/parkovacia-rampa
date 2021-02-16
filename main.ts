31daf6d58c43de082674108ffd74d29f1a20bf0d
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-16 20:12:53 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
-input.onButtonPressed(Button.A, function () {
-    Cervena()
-})
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 }
-input.onButtonPressed(Button.B, function () {
-    Zelena()
-})
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
@@ -22,7 +16,10 @@ basic.forever(function () {
     DigitalPin.P13,
     PingUnit.Centimeters
     )
-    basic.pause(200)
-    basic.showNumber(Vzdialenost)
-    basic.pause(200)
+    if (Vzdialenost <= 20) {
+        Zelena()
+    } else {
+        Cervena()
+    }
+    basic.pause(100)
 })
