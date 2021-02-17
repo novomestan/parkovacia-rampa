c7a3639aa6976930526819ea98454c8c7d89f191
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-17 20:20:26 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,26 @@
 function Zelena () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
     basic.pause(1000)
     wuKong.setServoAngel(wuKong.ServoList.S0, 45 / 1.5)
 }
 function Cervena () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showLeds(`
+        . # # # .
+        # . . # #
+        # . # . #
+        # # . . #
+        . # # # .
+        `)
     basic.pause(1000)
     wuKong.setServoAngel(wuKong.ServoList.S0, 135 / 1.5)
 }
